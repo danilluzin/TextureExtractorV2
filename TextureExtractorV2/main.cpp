b6ccc492cde70e0044728dce3595900129ead6d2
luzindan <luzindan@fit.cvut.cz>
2017-11-05 14:56:29 +0100

    Initial Commit

---
+++ TextureExtractorV2/main.cpp
@@ -0,0 +1,21 @@
+
+//  main.cpp
+//  TextureExtractor
+//
+//  Created by Danil Luzin on 07/10/2017.
+//  Copyright © 2017 Danil Luzin. All rights reserved.
+//
+
+#include <iostream>
+#include <glm/glm.hpp>
+
+#include <chrono>
+
+
+int main(int argc, const char * argv[]) {
+
+
+}
+
+
+
