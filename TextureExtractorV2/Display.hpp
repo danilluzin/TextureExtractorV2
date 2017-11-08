05de24536d20d7494f01c9d4d440e973f58789e1
luzindan <luzindan@fit.cvut.cz>
2017-11-08 15:36:16 +0100

    display class added from TEv1. Pig data added

---
+++ TextureExtractorV2/Display.hpp
@@ -0,0 +1,43 @@
+#ifndef Display_hpp
+#define Display_hpp
+
+
+#include <iostream>
+
+#include <GL/glew.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
+#include "Bitmap.hpp"
+#include "RenderContext.h"
+
+class Display : public RenderContext {
+    
+    SDL_Renderer * renderer;
+    
+    SDL_Window * window;
+    
+    bool isClosed;
+    
+    
+public:
+    Display( int width, int height, const std::string & title);
+    
+   ~Display();
+    
+    void update();
+    
+    bool isColsed();
+    
+    void clear(glm::vec4 color);
+    
+    void putPixel(int x, int y, glm::vec4 color);
+        
+    void loadBitmap(const Bitmap & bitmap );
+    
+    SDL_Renderer * getRenderer(){return renderer;}
+    
+private:
+ 
+};
+
+#endif /* Display_hpp */
